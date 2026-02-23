a8874eb2995a520736eca91b26fe337c77e5bfe0
Ярек <yarohatsune@gmail.com>
2026-02-23 19:04:56 +0500

    sidebar!!

--- js/index.js
+++ js/index.js
@@ -102,9 +102,31 @@ function isValidEmail(email) {
 }
 
 // knopka
-let sidebar = document.getElementById('sidebarToggle');
+let toggleButton = document.getElementById('sidebarToggle');
+let contentbox = document.querySelector('.scroll-box');
+let strelka = document.querySelector('.strelka')
+let txt = document.querySelectorAll('.text-link')
+let linkimg = document.querySelectorAll('.icon-container')
+let zagol = document.querySelector('.zagol')
+let zagcollaps = document.getElementById('zag-collaps')
 
+toggleButton.addEventListener('click', () => {
+    contentbox.classList.toggle('collapsed');
+    toggleButton.classList.toggle('collapsed');
+    strelka.classList.toggle('collapsed');
+    zagol.classList.toggle('collapsed');
+    zagcollaps.classList.toggle('collapsed');
 
-sidebar.addEventListener('click', function(){
-    console.log("lelele")
-})
+    linkimg.forEach(el => {
+        el.classList.toggle('collapsed')
+    })
+    txt.forEach(el => {
+        el.classList.toggle('collapsed')
+    })
+
+     if (contentbox.classList.contains('collapsed')) {
+        toggleButton.setAttribute('aria-label', 'Развернуть сайдбар');
+    } else {
+        toggleButton.setAttribute('aria-label', 'Свернуть сайдбар');
+    }
+});
